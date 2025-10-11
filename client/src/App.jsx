497ad942091f7a74dea0080f5a5c06f7591edf4b
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-10-11 19:40:59 +0600

    Navbar component is created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import Signup from './pages/Signup/Signup'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[7vw]'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/signup' element={<Signup />}/>
       </Routes>
     </div>
   )
