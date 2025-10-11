95b5941881e8acb1fad63df89d3b225aa3779da8
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-10-11 19:20:21 +0600

    Set up client routes and also page structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home/Home'
 
 function App() {
 
   return (
-    <div className='text-2xl'>
-      Hello world!
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[7vw]'>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
     </div>
   )
 }
