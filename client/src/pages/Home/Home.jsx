95998c625174d059be4b562ed4e49aee7a0b69cc
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-10-14 18:42:40 +0600

    NoteCard component is created

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import NoteCard from '../../components/Cards/NoteCard';
 
 const Home = () => {
     return (
-        <div>
-            Welcom to Home 
+        <div className='container px-4 sm:px-10 md:px-14'>
+            <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 mt-8'>
+                <NoteCard />
+                <NoteCard />
+                <NoteCard />
+                <NoteCard />
+            </div>
         </div>
     );
 };
